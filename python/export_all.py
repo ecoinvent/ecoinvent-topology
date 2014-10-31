18763aa69bda6743c3ce0ea3f349ffd54b0f6297
Chris Mutel <cmutel@gmail.com>
2014-10-31 13:01:04 +0100

    Add export script

---
+++ python/export_all.py
@@ -0,0 +1,28 @@
+import subprocess
+import shutil
+import os
+
+collections = ["aluminium" ,"americas-electricity" ,"countries" ,"legacy" ,"only-europe" ,"russia" ,"special" ,"un-regions" ,"un-subregions"]
+
+command = """ogr2ogr -f {format} output/{collection}.{extension} "PG:host=localhost dbname=eigeo user=ecoinvent" -sql "select * from final where collection = '{collection}'" -nln {layername} """
+
+shutil.rmtree("output")
+os.mkdir("output")
+
+for collection in collections:
+    subprocess.Popen(command.format(
+            format="GPKG",
+            collection=collection,
+            extension='gpkg',
+            layername=collection.replace("-", "_").replace(" ", "_"),
+        ), shell=True
+    )
+
+# print command.format(format="GeoJSON", collection=collection, extension='json')
+# formats = [
+#     # ogr format, extension
+#     ("GeoJSON", "geojson"),
+#     ("GPKG", "gpkg"),
+#     ("GPKG", "ESRI Shapefile"),
+# ]
+
