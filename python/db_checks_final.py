2b6c2a44b151bf0003a054bc3c23194b0adbf9cd
Chris Mutel <cmutel@gmail.com>
2014-11-17 11:47:37 +0100

    Test final geom validity

---
+++ python/db_checks_final.py
@@ -0,0 +1,14 @@
+import subprocess
+import sys
+
+valid_geom_final = "select name from final where not st_isvalid(geom)"
+
+def check_command(command, error, ok, expected="(0 rows)"):
+    p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+    result = p.communicate()[0]
+    if expected not in result:
+        raise sys.exit(error)
+    else:
+        print ok
+
+check_command(valid_geom_final, "Valid geometries", "All final geometries are valid")
