867b0a0fe72b18f70eb108a901c83d8569ea97e9
Chris Mutel <cmutel@gmail.com>
2014-11-21 17:16:54 +0100

    Add XML export

---
+++ python/write_xml.py
@@ -0,0 +1,123 @@
+from lxml.builder import ElementMaker
+from lxml.etree import tostring, fromstring
+import babel
+import csv
+import os
+import sys
+
+csv.field_size_limit(sys.maxsize)
+
+import unicodecsv
+
+
+
+XML_NS = "http://www.w3.org/XML/1998/namespace"
+
+# CSV column names:
+# 0: uuid
+# 1: name
+# 2: shortname
+# 3: geom
+# 4: isotwolettercode
+# 5: longitude
+# 6: isothreelettercode
+# 7: latitude
+
+class Ecospold2GeoExporter(object):
+    """Read in a CSV export from Postgresql and write out an Ecospold XML file."""
+    def __init__(self, output="Geographies.xml", languages=('es', 'ja', 'fr', 'de', 'zh')):
+        self.output_fp = os.path.realpath(os.path.join(
+            os.path.abspath(os.path.dirname(__file__)), "..", "output", output
+        ))
+        self.input_fp = os.path.realpath(os.path.join(
+            os.path.abspath(os.path.dirname(__file__)), "..", "output", "all.csv"
+        ))
+        self.languages = languages
+        self.language_dictionary = {}
+        for language in self.languages:
+            # TODO: Can split string based on en-US, etc.
+            locale = babel.Locale(language)
+            self.language_dictionary[language] = locale.territories
+
+        self.columns = [
+            u'id',
+            u'name',
+            u'shortname',
+            u'kml',
+            u'ISOTwoLetterCode',
+            u'longitude',
+            u'ISOThreeLetterCode',
+            u'latitude'
+        ]
+
+        self.input_reader = unicodecsv.reader(open(self.input_fp), encoding='utf-8')
+        self.create_document()  # Creates self.doc
+        self.write_document()
+
+    def create_document(self):
+        G = ElementMaker(namespace="http://www.EcoInvent.org/EcoSpold02",
+            nsmap={None : "http://www.EcoInvent.org/EcoSpold02"})
+        ROOT_ATTRS = {
+            "contextId":"DE659012-50C4-4e96-B54A-FC781BF987AB",
+            "majorRelease":"3",
+            "minorRelease": "0",
+            }
+        self.doc = G.validGeographies(
+            G.contextName("ecoinvent",
+                {"{%s}lang" % XML_NS: "en"}),
+            *[self.get_element(line, G) for line in list(self.input_reader)[:20]],
+            **ROOT_ATTRS)
+
+    def get_element(self, geo, G):
+        data = dict(zip(self.columns, geo))
+
+        NON_LANGUAGE_KEYS = {
+            u"id",
+            u'ISOTwoLetterCode',
+            u'UNCode',
+            u'longitude',
+            u'ISOThreeLetterCode',
+            u'latitude'
+        }
+        GEO_ATTRS = {key: value for key, value in data.iteritems() if key in NON_LANGUAGE_KEYS}
+
+        ELEMS = []
+
+        # Get name in all other languages
+        for language in self.languages:
+            if data['ISOTwoLetterCode'] in self.language_dictionary[language]:
+                ELEMS.append(G.name(self.language_dictionary[language][data['ISOTwoLetterCode']],
+                    {"{%s}lang" % XML_NS: language}))
+
+        ELEMS += [
+            G.name(data[u'name'], {u"{%s}lang" % XML_NS: u"en"}),
+            G.shortname(data[u'shortname'], {u"{%s}lang" % XML_NS: u"en"}),
+        ]
+
+        # Add description is available
+        # if geo.description:
+        #     ELEMS.append(
+        #         G.comment(
+        #             G.text(geo.description,
+        #                 {"{%s}lang" % XML_NS: "en", 'index': "0"}
+        #         )))
+
+        # Add KML fragment to names
+        ELEMS.append(
+            G.kml(
+                G.Document(
+                    G.name(data[u'name']),
+                    G.Placemark(
+                        fromstring(data[u'kml'])
+            ))))
+        return G.geography(*ELEMS, **GEO_ATTRS)
+
+    # TODO: Add RoW and Global
+
+    def write_document(self):
+        with open(self.output_fp, "w") as f:
+            f.write(tostring(self.doc, encoding='utf8'))
+
+
+if __name__ == "__main__":
+    es = Ecospold2GeoExporter()
