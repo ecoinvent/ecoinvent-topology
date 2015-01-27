59e88dd03ed008496c4a984210b4f73909e941ce
Chris Mutel <cmutel@gmail.com>
2015-01-27 15:17:41 +0100

    Exclude Cyprus no mans area

--- sql/fix_independent_islands.sql
+++ sql/fix_independent_islands.sql
@@ -22,4 +22,22 @@ update geometries set topogeom = CreateTopoGeom('ei_topo', 3, 1, (
     where name = 'Norway'
     and tname = 'ne_countries';
 
+-- Cyprus doesn't include no man's land (no economic activity)
+update geometries set topogeom = CreateTopoGeom('ei_topo', 3, 1, (
+    select TopoElementArray_Agg(elem) from (
+        select GetTopoGeomElements(topogeom) as elem
+            from geometries
+            where name = 'Cyprus'
+            and tname = 'ne_countries'
+        ) as t1
+        where elem not in (
+            select GetTopoGeomElements(topogeom) as foo
+                from geometries
+                where name = 'Cyprus No Mans Area'
+                and tname = 'ne_countries'
+        ))
+    )
+    where name = 'Cyprus'
+    and tname = 'ne_countries';
+
 COMMIT;
