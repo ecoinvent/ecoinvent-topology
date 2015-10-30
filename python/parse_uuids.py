c72a546d841d430e8a16576eb7ad40061fa38264
Chris Mutel <cmutel@gmail.com>
2015-10-30 17:03:47 +0100

    Update provinces to include country name

--- python/parse_uuids.py
+++ python/parse_uuids.py
@@ -13,11 +13,23 @@ BEGIN;\n"""
 FOOTER = u"""\nCOMMIT;\n"""
 
 TEMPLATE = u"""UPDATE final SET shortname='{shortname}', uuid='{uuid}' WHERE name = '{name}';"""
+CANADA_TEMPLATE = u"""UPDATE final SET shortname='{shortname}', uuid='{uuid}', name='Canada, {name}' WHERE name = '{name}';"""
+CHINA_TEMPLATE = u"""UPDATE final SET shortname='{shortname}', uuid='{uuid}', name='China, {name}' WHERE name = '{name}';"""
+
+def format_line(line, row):
+    if row == "Chinese provinces":
+        template = CHINA_TEMPLATE
+    elif row == "Canada":
+        template = CANADA_TEMPLATE
+    else:
+        template = TEMPLATE
+
+    return template.format(**line)
 
 
 data = json.load(open(os.path.join(os.getcwd(), "data", "config", "uuid-mapping.json")))
 
-sql = "\n".join([TEMPLATE.format(**line) for row in data for line in data[row]])
+sql = "\n".join([format_line(line, row) for row in data for line in data[row]])
 
 with codecs.open(os.path.join(os.getcwd(), "sql", "uuids.sql"), "w", encoding='utf8') as f:
     f.write(HEADER)
