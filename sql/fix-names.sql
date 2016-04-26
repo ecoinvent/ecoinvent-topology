a29aee2d70a13d0c06da472c64bc2ecf397ecf90
Chris Mutel <cmutel@gmail.com>
2016-04-26 14:11:56 +0200

    Fix names and shortnames for Ashmore and Indian Ocean Territories

--- sql/fix-names.sql
+++ sql/fix-names.sql
@@ -8,4 +8,6 @@ update final set name = 'Congo' where name = 'Republic of Congo';
 update final set name = 'Micronesia, Federated States of' where name = 'Federated States of Micronesia';
 update final set name = 'Serbia' where name = 'Republic of Serbia';
 update final set name = 'Tanzania' where name = 'United Republic of Tanzania';
+update final set shortname = 'AUS-AC', name = 'Australia, Ashmore and Cartier Islands' where name = 'Ashmore and Cartier Islands';
+update final set shortname = 'AUS-IOT', name = 'Australia, Indian Ocean Territories' where name = 'Indian Ocean Territories';
 COMMIT;
