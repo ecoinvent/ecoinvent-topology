43d0c30b0ee2a0af734246ae4a7c7e757a18e359
Chris Mutel <cmutel@gmail.com>
2023-11-08 14:50:41 +0100

    Rationalize SQL calls

--- sql/cleanup_final_table.sql
+++ sql/cleanup_final_table.sql
@@ -1,17 +1,15 @@
 SET CLIENT_ENCODING TO UTF8;
 SET STANDARD_CONFORMING_STRINGS TO ON;
 SET client_min_messages TO WARNING;
-UPDATE final SET isotwolettercode = NULL WHERE name = 'Somaliland';
-UPDATE final SET isothreelettercode = NULL WHERE name = 'Somaliland';
-UPDATE final SET isotwolettercode = NULL WHERE name = 'Siachen Glacier';
-UPDATE final SET isothreelettercode = NULL WHERE name = 'Siachen Glacier';
-UPDATE final SET isotwolettercode = NULL WHERE name = 'Spratly Islands';
-UPDATE final SET isothreelettercode = NULL WHERE name = 'Spratly Islands';
-UPDATE final SET isotwolettercode = NULL WHERE name = 'Coral Sea Islands';
-UPDATE final SET isothreelettercode = NULL WHERE name = 'Coral Sea Islands';
-UPDATE final SET isotwolettercode = NULL WHERE name = 'Clipperton Island';
-UPDATE final SET isothreelettercode = NULL WHERE name = 'Clipperton Island';
-UPDATE final SET isotwolettercode = NULL WHERE name = 'US Naval Base Guantanamo Bay';
-UPDATE final SET isothreelettercode = NULL WHERE name = 'US Naval Base Guantanamo Bay';
+UPDATE final SET isotwolettercode = NULL WHERE name in (
+    'Somaliland', 'Siachen Glacier', 'Spratly Islands', 'Coral Sea Islands',
+    'Clipperton Island', 'US Naval Base Guantanamo Bay',
+    'Australia, Ashmore and Cartier Islands'
+);
+UPDATE final SET isothreelettercode = NULL WHERE name in (
+    'Somaliland', 'Siachen Glacier', 'Spratly Islands', 'Coral Sea Islands',
+    'Clipperton Island', 'US Naval Base Guantanamo Bay',
+    'Australia, Ashmore and Cartier Islands'
+);
 BEGIN;
 COMMIT;
